638692b20e45d5a765e164a1ea0735bf9a6c413c
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-17 12:09:50 -0800

    add double sums with concat terminal message

--- Datatype_Practice/Program.cs
+++ Datatype_Practice/Program.cs
@@ -10,7 +10,13 @@ namespace Datatype_Practice
             int num2 = 12;
             int num3 = num1 + num2;
             Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + num3);
-            Console.Read();
+            
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumd = d1 + d2;
+            Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sumd);
+
         }
     }
 }
