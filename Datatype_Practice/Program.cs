330a8d550f1931ddd349c6aa55b6a62a1cb30099
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-17 12:16:20 -0800

    addition with float datatypes

--- Datatype_Practice/Program.cs
+++ Datatype_Practice/Program.cs
@@ -9,13 +9,22 @@ namespace Datatype_Practice
             int num1 = 13;
             int num2 = 12;
             int num3 = num1 + num2;
+            int num4;
             Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + num3);
             
 
             double d1 = 3.5;
             double d2 = 1.337;
             double sumd = d1 + d2;
+
+            num4 = 15;
+            Console.WriteLine(num4);
             Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sumd);
+
+            float f1 = 3.5f;
+            float f2 = 2.5f;
+            float sumF = f2 + f1;
+            Console.WriteLine(sumF);
 
         }
     }
